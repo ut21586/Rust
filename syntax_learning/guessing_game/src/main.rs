9b6abe9d113d084cddf4d0e965110e97706b87e5
Eason Yilin  Zhou <ut21586@bristol.ac.uk>
2024-03-23 16:42:26 +0000

    update

--- syntax_learning/guessing_game/src/main.rs
+++ syntax_learning/guessing_game/src/main.rs
@@ -1,7 +1,11 @@
 //use std::io "io" is library come from std(standard library) and it used to obtain use input and then print the result as output so we need ot vbring the io input/output library into scope
 //rust each line of code end with";"
 //scope 界限
-
+//prelude: its a set that rust has a set of items defined in the standard library that it brings into the scope of every program.
+//"use" if a type you want to use isn't in the prelude, you have to bring that type into scope explicitly with a use statement, using the std::io library provides you with a number of udeful features, including the ability to accept user input.
+// "let mut guess = String::new();" create a variable to store the use input.
+//"let apple = 5;" use "let" statetement to create a new variable and assign a new value with it.
+//you still
 
 
 
